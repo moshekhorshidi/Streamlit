4ad4111065d1a3f0b68273b2040bdc485815d0eb
Moshe Khorshidi Code Jungle <moshekhorshidi@gmail.com>
2023-12-14 19:35:57 +0200

    Update report_manager_streamlit_app.py

--- report_manager_streamlit_app.py
+++ report_manager_streamlit_app.py
@@ -371,7 +371,7 @@ def Customers_Taxes_Report():
 # UI info welcome text
 st.title("Azure Store Report Manager UI")
 st.write("Welcome to report manager and azure database exploration ***Web application***, developed by **Moshe Khorshidi**.")
-st.write("***Phone: +972-526775714 , eMail: MosheKhorshidi@gmail.com***, ***LinkedIn:*** [My LinkedIn Profile 👋](https://www.linkedin.com/in/moshe-khorshidi-sql-python-tableau-data-engineering-data-science/) ")
+st.write("***Phone: +972-526775714 , eMail: MosheKhorshidi@gmail.com***, ***LinkedIn:*** [My LinkedIn Profile 👋](https://www.linkedin.com/in/moshe-khorshidi-sql-python-tableau-data-analysis-bi-engineering-curiosity-for-innovation/) ")
 
 # information message to user on test connection on azure database
 st.info("Users can be **offline and get un-updated data from azure server**, for better usage test your connection first.")
